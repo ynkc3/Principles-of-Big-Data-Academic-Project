559c1b3a337f800ad1ff28dedde72672dad42876
ynkc3 <61433951+ynkc3@users.noreply.github.com>
2020-07-30 23:35:56 -0500

    Phase1

---
+++ Phase 1/Source/twitter_credentials.py
@@ -0,0 +1,9 @@
+# User Credentials variables to access Twitter API
+
+#Access Tokens
+ACCESS_TOKEN = "838364671014993920-UJ9poZckaunMsoKBcbruOkfTktQb2ak"
+ACCESS_TOKEN_SECRET = "HF9TRTiBqgCBMD4G6VwsdZnmNEmljdjkpZ7ZE9KhskyQH"
+
+#Consumer API Keys
+CONSUMER_KEY = "GmWpny3Lj2Rl14cykR3fYYtyJ"
+CONSUMER_SECRET = "yT2IWtBDNrMjFwtSmObYJ3qJpTgAWuREkWmzkYlgkqOb329V17"
